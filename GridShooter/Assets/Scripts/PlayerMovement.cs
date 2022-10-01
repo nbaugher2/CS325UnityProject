7772e37d5578baf854c74bd19a4078de7ed40fed
nbaugher2 <108693568+nbaugher2@users.noreply.github.com>
2022-09-30 21:41:01 -0400

    Added project to repo
    Added Unity Project

---
+++ GridShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.transform.position = new Vector3(0.5f,0.5f,0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveUpdate();
+    }
+    
+    /*Function which updates the player position based on input to WASD keys*/
+    void PlayerMoveUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && player.transform.position.x > -5.5f)
+        {
+            /*move left*/
+            player.transform.position = new Vector3(-1.0f, 0.0f, 0.0f) + player.transform.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && player.transform.position.x < 6.5f)
+        {
+            /*move right*/
+            player.transform.position = new Vector3(1.0f, 0.0f, 0.0f) + player.transform.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && player.transform.position.y < 2.5)
+        {
+            /*move up*/
+            player.transform.position = new Vector3(0.0f, 1.0f, 0.0f) + player.transform.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && player.transform.position.y > -2.5)
+        {
+            player.transform.position = new Vector3(0.0f, -1.0f, 0.0f) + player.transform.position;
+        }
+    }
+}
