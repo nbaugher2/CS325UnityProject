bcb334b7c0236b880e49044b43c95115844d4039
nbaugher2 <108693568+nbaugher2@users.noreply.github.com>
2022-10-04 22:24:41 -0400

    Added medic spawners
    Added script which spawns enemies and also decrements the amount of time it takes to spawn an enemy over time

--- GridShooter/Assets/Scripts/MedicSpawner.cs
+++ GridShooter/Assets/Scripts/MedicSpawner.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class MedicSpawner : MonoBehaviour
 {
+    private IEnumerator spawnMedicHolder;
+    private IEnumerator increaseDifficultyHolder;
+    public GameObject medicPrefab;
+    float spawnTime = 5.0f;
+    public float xMax;
+    public float xMin;
+    public float yMax;
+    public float yMin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnMedicHolder = spawnMedic();
+        increaseDifficultyHolder = increaseDifficulty();
+        StartCoroutine(increaseDifficultyHolder);
+        StartCoroutine(spawnMedicHolder);
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator spawnMedic()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            Instantiate(medicPrefab, new Vector3(Random.Range(xMin,xMax), Random.Range(yMin,yMax),-1.0f), Quaternion.identity);
+        }
     }
+
+    private IEnumerator increaseDifficulty()
+    {
+        while (true)
+        {
+            if (spawnTime > .75f)
+            {
+                yield return new WaitForSeconds(10.0f);
+                spawnTime -= .5f;
+            }
+            else
+            {
+                yield break;
+            }
+        }
+    }
+
 }
